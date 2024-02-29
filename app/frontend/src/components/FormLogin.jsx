e5e46cd144446ed02274adabdb9df7bcbd87401a
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2024-02-29 19:26:48 -0300

    Adiciona a captura de informações de login ao formulario de login

--- app/frontend/src/components/FormLogin.jsx
+++ app/frontend/src/components/FormLogin.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function FormLogin() {
+  const [emailLogin, setEmail] = useState('');
+  const [passwordLogin, setpassword] = useState('');
+  const [isDisabled, setStateButton] = useState(true);
+
+  useEffect(() => {
+    const regexEmail = /^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+    const passwordLength = 7;
+    setStateButton(regexEmail.test(emailLogin) && passwordLogin.length > passwordLength);
+  }, [emailLogin, passwordLogin]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = ({ target }) => {
+    if (target.name === 'email') setEmail(target.value);
+    if (target.name === 'password') setpassword(target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={ (e) => handleSubmit(e) }>
       <h3>Login</h3>
       <label htmlFor="email">Email</label>
-      <input name="email" id="email" />
+      <input
+        value={ emailLogin }
+        onChange={ handleChange }
+        name="email"
+        id="email"
+      />
       <label htmlFor="password">Password</label>
-      <input name="password" id="password" />
+      <input
+        value={ passwordLogin }
+        onChange={ handleChange }
+        name="password"
+        id="password"
+      />
+      <button
+        type="submit"
+        disabled={ !isDisabled }
+      >
+        Login
+      </button>
     </form>
   );
 }
